3b334b6e63c00263afab36bb2b1cf65c46b8b24c
Samuel Hardy <sam.s.hardy1@gmail.com>
2017-08-18 17:46:10 -0600

    Pushing to GitHub

--- TableViewExcercise/TableViewController.swift
+++ TableViewExcercise/TableViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
